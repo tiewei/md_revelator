b923874a3498c746cbef1986a9517ab5ce90d5cc
Wei Tie <nuaafe@gmail.com>
2014-03-16 20:49:03 +0800

    Using input file name as the generated html name

--- lib/md_revelator/redcarpet_revelator.rb
+++ lib/md_revelator/redcarpet_revelator.rb
@@ -9,7 +9,9 @@ module Md::Revelator
       output = Redcarpet::Markdown.new(Md::Revelator::RevealRender, extends).render(input)
       reveal_js = "#{File.dirname(__FILE__)}/../../reveal.js-2.6.1/*"
       FileUtils.cp_r(Dir.glob(reveal_js) , output_dir)
-      File.open("#{output_dir}/index.html", "w"){|f| f.write output}
+      output_name = File.basename(input_file, File.extname(input_file))
+      File.open("#{output_dir}/#{output_name}.html", "w"){|f| f.write output}
+      return "#{output_dir}/#{output_name}.html"
     end
   end
 end
